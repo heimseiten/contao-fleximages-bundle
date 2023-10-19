aa097df41597bb91079cdcf9f89ca3bf10fda68d
Niels <info@heimseiten.de>
2023-10-19 11:57:04 +0200

    Update tl_content.php

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -11,6 +11,11 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['flex_gallery_image_height'] = [
     'sql'       => "char(11) NOT NULL default ''" 
 ];
 
+$GLOBALS['TL_DCA']['tl_content']['fields']['perRow']['eval']['tl_class'] = 'w25 hidden';
+$GLOBALS['TL_DCA']['tl_content']['fields']['perPage']['eval']['tl_class'] = 'w25';
+$GLOBALS['TL_DCA']['tl_content']['fields']['numberOfItems']['eval']['tl_class'] = 'w25';
+$GLOBALS['TL_DCA']['tl_content']['fields']['fullsize']['eval']['tl_class'] = 'm12 w50';
+
 PaletteManipulator::create()
     ->addField('flex_gallery_image_height', 'image_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('gallery', 'tl_content') 
