5a4fa53534b91e0d85eddb4bf0c46856891994b6
Niels <info@heimseiten.de>
2023-10-23 13:59:14 +0200

    Update tl_content.php

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -3,20 +3,19 @@
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 use Contao\DataContainer;
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['flex_gallery_image_height'] = [ 
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['flex_gallery_image_height'], 
-    'inputType' => 'text', 
+$GLOBALS['TL_DCA']['tl_content']['fields']['use_flex_gallery_image'] = [ 
+    'label'     => &$GLOBALS['TL_LANG']['tl_content']['use_flex_gallery_image'], 
+    'inputType' => 'checkbox', 
     'search'    => true,
     'eval'      => array('tl_class' => 'w50'),
-    'sql'       => "char(11) NOT NULL default ''" 
+    'sql'       => array('type' => 'boolean', 'default' => false)
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['perRow']['eval']['tl_class'] = 'w25 hidden';
 $GLOBALS['TL_DCA']['tl_content']['fields']['perPage']['eval']['tl_class'] = 'w25';
 $GLOBALS['TL_DCA']['tl_content']['fields']['numberOfItems']['eval']['tl_class'] = 'w25';
-$GLOBALS['TL_DCA']['tl_content']['fields']['fullsize']['eval']['tl_class'] = 'm12 w50';
 
 PaletteManipulator::create()
-    ->addField('flex_gallery_image_height', 'image_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('use_flex_gallery_image', 'image_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('gallery', 'tl_content') 
 ;
