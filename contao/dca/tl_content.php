df2f695a55373443a44c2ff2ab3e0adb8fe3b330
Niels <info@heimseiten.de>
2023-10-24 10:17:08 +0200

    Update tl_content.php

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -3,8 +3,8 @@
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 use Contao\DataContainer;
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['use_flex_gallery_image'] = [ 
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['use_flex_gallery_image'], 
+$GLOBALS['TL_DCA']['tl_content']['fields']['use_fleximages'] = [ 
+    'label'     => &$GLOBALS['TL_LANG']['tl_content']['use_fleximages'], 
     'inputType' => 'checkbox', 
     'search'    => true,
     'eval'      => array('tl_class' => 'w50'),
@@ -12,6 +12,6 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['use_flex_gallery_image'] = [
 ];
 
 PaletteManipulator::create()
-    ->addField('use_flex_gallery_image', 'image_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('use_fleximages', 'image_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('gallery', 'tl_content') 
 ;
